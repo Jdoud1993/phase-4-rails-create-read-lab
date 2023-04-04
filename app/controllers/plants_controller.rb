a772b746a3102c48cc01de04082acd591bf257d1
Joshua Doud <bigjj3393@yahoo.com>
2023-04-03 18:09:42 -0700

    commit tree 0baee2fcf9b2a9a061b983c4bbc490a47c8e2617

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,16 @@
+class PlantsController < ApplicationController
+
+    wrap_parameters format: []
+
+    def index
+        render json: Plant.all
+    end
+
+    def show
+        render json: Plant.find_by(id: params[:id])
+    end
+
+    def create
+        render json: Plant.create(params.permit(:name, :image, :price)), status: :created
+    end
+end
